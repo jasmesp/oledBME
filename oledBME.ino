2274f0749b1402040d945f36a934bd836a1e33e0
jasmesp <jasmes@gmail.com>
2021-01-01 17:08:41 -0500

    Add project files.

---
+++ oledBME.ino
@@ -0,0 +1,22 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_BME280.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET	4
+
+Adafruit_SSD1306 display(SCREEN)
+
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
